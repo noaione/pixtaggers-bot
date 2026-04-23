32061cd1c52502328d38c29637fd753a984c8660
noaione <noaione0809@gmail.com>
2026-04-23 21:11:29 +0700

    chore: change default port

--- main.py
+++ main.py
@@ -336,4 +336,4 @@ def manual_tag_update(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=44823, lifespan="on")
+    uvicorn.run(app, host="127.0.0.1", port=23810, lifespan="on")
