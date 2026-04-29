33f83019a5ae7c8bc895809d0961ef46ba5626ab
noaione <noaione0809@gmail.com>
2026-04-29 19:02:10 +0700

    fix: use progressive q95 jpeg for video thumb

--- main.py
+++ main.py
@@ -103,7 +103,9 @@ async def maybe_upload_video_frame_as_thumbnail(post: SimplePost, client: Szurub
 
     thumb_img = resize_by_longest_side(img, config_data.thumbnails.target_size)
     output_buffer = BytesIO()
-    await asyncio.to_thread(lambda: thumb_img.save(output_buffer, format="PNG", optimize=True))
+    await asyncio.to_thread(
+        lambda: thumb_img.save(output_buffer, format="JPG", quality=95, optimize=True, progressive=True)
+    )
     thumb_bytes = output_buffer.getvalue()
     print(f"Updating video thumbnail for post ID {post['id']}...")
     try:
